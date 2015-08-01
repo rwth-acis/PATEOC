f44a68179eb46b1dbe82898d88fd00a1580968f4
Stephen <Stephen>
2015-08-01 16:38:14 +0200

    modular update v2.2

--- src/main/sg/javapackage/CommunityEvolutionPrediction.java
+++ src/main/sg/javapackage/CommunityEvolutionPrediction.java
@@ -3,15 +3,8 @@ package main.sg.javapackage;
 import java.io.File;
 import main.sg.javapackage.domain.GlobalVariables.Algorithm;
 import main.sg.javapackage.domain.GlobalVariables.Dataset;
-import main.sg.javapackage.graph.PreProcessing;
 import main.sg.javapackage.logging.Logger;
-import main.sg.javapackage.ocd.OverlapCommunityDetection;
-import main.sg.javapackage.sna.EvolutionDetection;
-import main.sg.javapackage.sna.SocialNetworkAnalysis;
 import main.sg.javapackage.utils.InputManager;
-import main.sg.javapackage.weka.PredictiveAnalysis;
-import main.sg.javapackage.weka.SupervisedLearning;
-
 import org.ini4j.Ini;
 
 
@@ -36,7 +29,7 @@ public class CommunityEvolutionPrediction {
 	public static void main(String[] args) throws Exception{
 		
 		//Declarations
-		final double version = 2.1;
+		final double version = 2.2;
 		boolean isWeighted = false;
 		final int totalTimesteps;
 		final long programStartTime = System.currentTimeMillis();
@@ -112,37 +105,9 @@ public class CommunityEvolutionPrediction {
 		System.out.println("Weighted	  :: "+isWeighted);
 		
 		/*-----------------------------------------------------------------------------------------------------------------*/
-		
-		Logger.writeToLogln("/*------------------Pre-Processor---------------------*/");
-		PreProcessing prep = new PreProcessing(InputPath, totalTimesteps, selectedAlgo);
-		prep.processInputNodeList();
-		Logger.writeToLogln("");
-		System.out.println("Stage 1- Pre-Processor : Complete");
-
-		Logger.writeToLogln("/*-----------Overlapping-Community-Detector-----------*/");
-		OverlapCommunityDetection ocd = new OverlapCommunityDetection(selectedAlgo, isWeighted, totalTimesteps);
-		ocd.performOverlapCommunityDectection(InputPath,OCDPath);
-		Logger.writeToLogln("");
-		System.out.println("Stage 2- Community Dectector : Complete");
-
-		Logger.writeToLogln("/*---------------Statistical-Extractor----------------*/");
-		SocialNetworkAnalysis sna = new SocialNetworkAnalysis();
-		sna.extractAnalytics();
-		System.out.println("Stage 3a- Statistical Extractor : Complete");
-
-		EvolutionDetection ged = new EvolutionDetection();
-		ged.onetomanyCommunityEvolutionTracking();
-		ged.recursiveCommunityEvolutionTracking();
-		System.out.println("Stage 3b- Group Evolution Discoverer : Complete");
-
-		Logger.writeToLogln("/*----------------Predictive-Analysis-----------------*/");
-		PredictiveAnalysis learning = new PredictiveAnalysis();
-		learning.populateArffFile();
-		System.out.println("Stage 4a- Prediction Formulator : Complete");
-
-		SupervisedLearning classify = new SupervisedLearning();
-		classify.performPredictiveAnalysis();
-		System.out.println("Stage 4b- Supervised Learner : Complete");
+		ModularPipeline pipeline = new ModularPipeline(InputPath, totalTimesteps, selectedAlgo, isWeighted, OCDPath);
+		pipeline.CommunityEvolutionPipeline();
+		/*-----------------------------------------------------------------------------------------------------------------*/
 
 		//System Runtime
 		System.out.println("-------------------------------------------");
