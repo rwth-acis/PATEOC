6e46829c65ef6d71db010e4bb34a83485525b0e8
Stephen <Stephen>
2015-09-22 14:28:01 +0200

    removed range to allow any metric to be plotted

--- src/main/sg/javapackage/visualize/ChartVisualization.java
+++ src/main/sg/javapackage/visualize/ChartVisualization.java
@@ -129,8 +129,8 @@ public class ChartVisualization extends JFrame{
 	
 	private void setLineChartOptions(JFreeChart chart) {
 		XYPlot plot = chart.getXYPlot();
-		ValueAxis rangeAxis = plot.getRangeAxis();
-		rangeAxis.setRange(40.0, 100.0);
+		//ValueAxis rangeAxis = plot.getRangeAxis();
+		//rangeAxis.setRange(40.0, 100.0);
 		
 		ValueAxis domainAxis = plot.getDomainAxis();
 		domainAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
@@ -141,7 +141,7 @@ public class ChartVisualization extends JFrame{
 		renderer.setBaseStroke(new BasicStroke(3.0f));
 		renderer.setAutoPopulateSeriesStroke(false);
 
-		// label the points
+		// sets the points' label
         NumberFormat format = NumberFormat.getNumberInstance();
         format.setMaximumFractionDigits(2);
         XYItemLabelGenerator generator =
@@ -150,7 +150,7 @@ public class ChartVisualization extends JFrame{
                 format, format);
         renderer.setBaseItemLabelGenerator(generator);
         renderer.setBaseItemLabelsVisible(true);
-        renderer.setBaseItemLabelFont(new Font("SansSerif", Font.PLAIN, 14));
+        renderer.setBaseItemLabelFont(new Font("SansSerif", Font.PLAIN, 10));
         
 		// sets paint color for plot outlines
 		plot.setOutlinePaint(Color.BLUE);
